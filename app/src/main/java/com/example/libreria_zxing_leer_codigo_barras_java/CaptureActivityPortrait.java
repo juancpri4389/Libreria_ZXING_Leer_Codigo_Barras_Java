f433544e47cddaee8541cd3cd87a55f23feb61f9
juancpri4389 <129116527+juancpri4389@users.noreply.github.com>
2023-03-27 19:46:42 -0500

    scan
    scan_android_studio

---
+++ app/src/main/java/com/example/libreria_zxing_leer_codigo_barras_java/CaptureActivityPortrait.java
@@ -0,0 +1,6 @@
+package com.example.libreria_zxing_leer_codigo_barras_java;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+}
